677965acb43100d98d9412c2888bbf98387e5fd9
Shaan Yadav <39740442+Shaan106@users.noreply.github.com>
2023-07-14 15:38:51 +0100

    finished task list hw

--- week_1/day_5/main.js
+++ week_1/day_5/main.js
@@ -1,1 +1,25 @@
 console.log("connected")
+
+let taskInfo = document.getElementById("task-info");
+let taskTable = document.getElementById("task-table");
+
+document.querySelector('#add-task-button').addEventListener('click', () => {
+
+    const tr = document.createElement('tr');
+
+    let newTask = document.createElement('td');
+    let newCheckbox = document.createElement('td');
+    let newDelButton = document.createElement('td');
+
+    newTask.innerHTML = taskInfo.value;
+    newCheckbox.innerHTML = `<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>`;
+    newDelButton.innerHTML = `<button type="button" class="btn-close" aria-label="Close"></button>`;
+
+    tr.appendChild(newTask);
+    tr.appendChild(newCheckbox);       
+    tr.appendChild(newDelButton);
+
+    taskTable.appendChild(tr);
+
+    console.log(taskInfo.value);
+});

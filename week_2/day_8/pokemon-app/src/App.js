0e1d99f0534b1940aad6d67bd0e305cb62ab9f79
Shaan Yadav <39740442+Shaan106@users.noreply.github.com>
2023-07-20 10:02:04 +0100

    pokemon api basic request

---
+++ week_2/day_8/pokemon-app/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+
+import { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  //base url for api req
+  //const url = 'https://jsonplaceholder.typicode.com/';
+  const url = 'https://pokeapi.co/api/v2/';
+
+  //array for all api returns
+  const [pokeInfo, setPokeInfo] = useState([]);
+  const [pokeSpecies, setPokeSpecies] = useState("");
+
+  // function to fetch api requests
+  async function fetchPokemon() {
+    
+    //fetch Library
+    const res = await fetch(url + 'pokemon/pikachu', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/text',
+      },
+    });
+    const data = await res.json();
+
+    // console.log(data);
+    // console.log(data.species.name);
+
+    setPokeInfo(data);
+    setPokeSpecies(data.species);
+
+    console.log(pokeInfo["species.name"])
+
+  }
+
+  return (
+    <div className="text-center">
+      <button className="btn btn-primary mt-3" onClick={fetchPokemon}>
+        Fetch Posts
+      </button>
+
+      <div>
+        <div>{pokeInfo.id}</div>
+      </div>
+      
+      <div>
+        <div>{pokeSpecies.name}</div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
